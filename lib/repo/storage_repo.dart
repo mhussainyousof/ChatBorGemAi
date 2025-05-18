4c31fbf8cd5d01acc7b183d92a8eb45382db1ee9
Hussain Yousof <mhussainyousof@gmail.com>
2025-05-18 10:16:53 +0330

    Implementing repos

---
+++ lib/repo/storage_repo.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:image_picker/image_picker.dart';
+
+@immutable
+class StorageRepository {
+  final FirebaseStorage _storage;
+
+   StorageRepository({
+    FirebaseStorage? storage,
+  }) : _storage = storage ?? FirebaseStorage.instance;
+
+  Future<String> saveImageToStorage({
+    required XFile image,
+    required String messageId,
+  }) async {
+    try {
+      final ref = _storage.ref('images').child(messageId);
+      final snapshot = await ref.putFile(File(image.path));
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      throw StorageException('Failed to upload image: ${e.toString()}');
+    }
+  }
+}
+
+class StorageException implements Exception {
+  final String message;
+  StorageException(this.message);
+
+  @override
+  String toString() => 'StorageException: $message';
+}
