9e9d920513d93ed09814c342b3edffbefae41224
Hussain Yousof <mhussainyousof@gmail.com>
2025-05-26 18:22:46 +0330

    implementing a better gemini model

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gemini_chat_bot/provider/providers.dart';
@@ -23,12 +24,14 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   XFile? selectedImage;
   late final ScrollController _scrollController;
   bool _showScrollToBottomBtn = false;
+   bool _isSending = false;
+
 
   @override
   void initState() {
     _messageController = TextEditingController();
     _scrollController = ScrollController();
-
+ SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     _scrollController.addListener(() {
       final atBottom = _scrollController.offset <=
           _scrollController.position.minScrollExtent + 20;
@@ -44,7 +47,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   Future<void> _pickImage() async {
-    final pickedImage = await pickImage(); // your own util function
+    final pickedImage = await pickImage(); 
     if (pickedImage != null) {
       setState(() {
         selectedImage = pickedImage;
@@ -61,167 +64,182 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
+    return
         Scaffold(
+          floatingActionButton: _showScrollToBottomBtn  ? Align(
+            
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 90),
+              child: FloatingActionButton.small(
+                
+                backgroundColor: Colors.black45,
+                foregroundColor: Colors.white,
+                shape: const CircleBorder(),
+                onPressed: () {
+                  _scrollController.animateTo(
+                    0,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+                child: const Icon(Iconsax.arrow_down_1),
+              ),
+            ),
+          ): null,
           backgroundColor: Colors.white,
           resizeToAvoidBottomInset: true,
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 12,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Consumer(builder: (context, ref, child) {
-                    return Row(
+          body: AnnotatedRegion<SystemUiOverlayStyle>(
+             value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark, 
+          systemNavigationBarColor: Colors.transparent,
+          systemNavigationBarIconBrightness: Brightness.dark,
+        ),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Consumer(builder: (context, ref, child) {
+                      return Row(
+                        children: [
+                          const Text(
+                            "Let's Talk 🐦‍🔥 ",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          const Spacer(),
+                          const Text('Logout'),
+                          IconButton(
+                            onPressed: () {
+                              ref.read(authProvider).signout();
+                            },
+                            icon: const Icon(
+                              Iconsax.logout,
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+                    // Message List
+                    Expanded(
+                      child: MessagesList(
+                        scrollController: _scrollController,
+                        userId: FirebaseAuth.instance.currentUser!.uid,
+                      ),
+                    ),
+            
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          "Let's Talk 🐦‍🔥 ",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        const Spacer(),
-                        const Text('Logout'),
-                        IconButton(
-                          onPressed: () {
-                            ref.read(authProvider).signout();
-                          },
-                          icon: const Icon(
-                            Iconsax.logout,
+                        if (selectedImage != null)
+                          Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.file(
+                                  File(selectedImage!.path),
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Positioned(
+                                top: 8,
+                                right: 8,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedImage = null;
+                                    });
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(0.5),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(4.0),
+                                      child: Icon(Icons.close,
+                                          color: Colors.white, size: 15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 3,
+                          ),
+                          margin: EdgeInsets.only(
+                              top: selectedImage != null ? 5 : 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade100,
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Row(
+                            children: [
+                              //! Message Text field
+                              Expanded(
+                                child: TextField(
+                                  maxLines: null,
+                                  keyboardType: TextInputType.multiline,
+                                  controller: _messageController,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Ask any question',
+                                    hintStyle: TextStyle(
+                                      color: Colors.grey.shade500,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                              ),
+            
+                              // Image Button
+                              IconButton(
+                                onPressed: _pickImage,
+                                icon: const Icon(
+                                  Iconsax.gallery,
+                                ),
+                              ),
+            
+                              //! Send Button
+                              IconButton(
+                                  onPressed: _isSending ? null :   sendMessage,
+                                  icon:  _isSending ? const SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                     child: CircularProgressIndicator(strokeWidth: 2),
+                                  )  : const Icon(Iconsax.send_1)),
+                            ],
                           ),
                         ),
                       ],
-                    );
-                  }),
-                  // Message List
-                  Expanded(
-                    child: MessagesList(
-                      scrollController: _scrollController,
-                      userId: FirebaseAuth.instance.currentUser!.uid,
                     ),
-                  ),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (selectedImage != null)
-                        Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.file(
-                                File(selectedImage!.path),
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                            Positioned(
-                              top: 8,
-                              right: 8,
-                              child: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    selectedImage = null;
-                                  });
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.black.withOpacity(0.5),
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: const Padding(
-                                    padding: EdgeInsets.all(4.0),
-                                    child: Icon(Icons.close,
-                                        color: Colors.white, size: 15),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 3,
-                        ),
-                        margin: EdgeInsets.only(
-                            top: selectedImage != null ? 5 : 20),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Row(
-                          children: [
-                            //! Message Text field
-                            Expanded(
-                              child: TextField(
-                                maxLines: null,
-                                keyboardType: TextInputType.multiline,
-                                controller: _messageController,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Ask any question',
-                                  hintStyle: TextStyle(
-                                    color: Colors.grey.shade500,
-                                    fontStyle: FontStyle.italic,
-                                  ),
-                                ),
-                              ),
-                            ),
-
-                            // Image Button
-                            IconButton(
-                              onPressed: _pickImage,
-                              icon: const Icon(
-                                Iconsax.gallery,
-                              ),
-                            ),
-
-                            //! Send Button
-                            IconButton(
-                                onPressed: sendMessage,
-                                icon: const Icon(Iconsax.send_1)),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
+                
               ),
             ),
           ),
-        ),
-        if (_showScrollToBottomBtn)
-  Align(
-    alignment: Alignment.bottomCenter,
-    child: Padding(
-      padding: const EdgeInsets.only(bottom: 140), 
-        child: FloatingActionButton.small(
-          shape:const CircleBorder(),
-          backgroundColor: Colors.black45, 
-          foregroundColor: Colors.white,
-          onPressed: () {
-            _scrollController.animateTo(
-              0,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeOut,
-          );
-        },
-        child: const Icon(Iconsax.arrow_down_1),
-      ),
-    ),
-  ),
-      ],
-    );
+        );
   }
 
   Future sendMessage() async {
     final message = _messageController.text.trim();
 
     if (message.isEmpty && selectedImage == null) return;
+      setState(() {
+    _isSending = true;
+      _messageController.clear();
+  });
 
     try {
       if (selectedImage != null) {
@@ -234,14 +252,17 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         await ref
             .read(chatProvider)
             .sendTextMessage(textPrompt: message, apiKey: apiKey);
-      }
-      _messageController.clear();
       setState(() {
         selectedImage = null;
       });
+      }
     } catch (e) {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text('Error: $e')));
+    }finally{
+      setState(() {
+        _isSending = false;
+      });
     }
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gemini_chat_bot/provider/providers.dart';
+import 'package:gemini_chat_bot/provider/ui_state_providers.dart';
 
 
 class LoginScreen extends ConsumerWidget {
@@ -8,51 +9,106 @@ class LoginScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final size = MediaQuery.of(context).size;
+    final isLoading = ref.watch(isLoadingProvider);
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'Sign In With Google',
-            style: TextStyle(
-              fontSize: 50,
+      body:  Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Card(
+            color: Colors.white70,
+            elevation: 15,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(
-            width: double.infinity,
-            height: 100,
-          ),
-          Card(
-            elevation: 10,
-            child: SizedBox(
-              height: 60,
-              width: 300,
-              child: TextButton(
-                onPressed: () async {
-                  await ref.read(authProvider).signInWithGoogle();
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      'assets/icons/googlelogo.png',
-                      fit: BoxFit.contain,
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Welcome to Promptly',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Text(
-                      'Login with Google',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'Your AI-powered\nconversation partner',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.blueGrey,
+                      fontStyle: FontStyle.italic,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height:50),
+                  const Text(
+                    'Sign in to continue',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blueGrey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10),
+                  isLoading ? const CircularProgressIndicator() :
+                  SizedBox(
+                    width: size.width > 400 ? 300 : double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () async {
+                        ref.read(isLoadingProvider.notifier).state = true;
+                        try{
+                        await ref.read(authProvider).signInWithGoogle();
+                        }finally{
+                          ref.read(isLoadingProvider.notifier).state = false;
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icons/googlelogo.png',
+                            height: 24,
+                          ),
+                          const SizedBox(width: 12),
+                          const Text(
+                            'Sign in with Google',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
